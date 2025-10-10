1b597a2d52a5f952e78ec94758bc6213f2d90959
Jason Leong <jleongdh@gmail.com>
2025-10-10 12:13:18 +1030

    Backend(learn): Understand db.js

--- backend/db.js
+++ backend/db.js
@@ -6,12 +6,12 @@ const pool = new Pool({
   user: process.env.DB_USER || "jleong_23",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "attendance",
-  password: process.env.DB_PASSWORD || "@0128193303Postgres",
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
 });
 
 // Test the connection
-pool.query("SELECT NOW()", (err, res) => {
+pool.query("SELECT NOW()", (err) => {
   if (err) {
     console.error("Database connection error:", err);
   } else {
