da6307f86feb89970bf33c5bbd1892b1eb1598a7
Jason Leong <jleongdh@gmail.com>
2025-10-10 12:28:47 +1030

    Backend(learn): Understanad init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS kids;
 
 -- Create kids table
 CREATE TABLE kids (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    photo TEXT,
+    id SERIAL PRIMARY KEY, --Auto incrementing ID
+    name TEXT NOT NULL, --name is required
+    photo TEXT, --path/url of photo
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
@@ -14,14 +14,14 @@ CREATE TABLE kids (
 -- Create attendance table
 CREATE TABLE attendance (
     id SERIAL PRIMARY KEY,
-    kidId INT NOT NULL,
+    kidId INT NOT NULL, --foreign key to kids table
     name TEXT,
     week INT NOT NULL,
-    term INT DEFAULT 1,
+    term INT DEFAULT 1, --default term starts at 1
     present BOOLEAN DEFAULT FALSE,
     reason TEXT,
     photo TEXT,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (kidId) REFERENCES kids(id) ON DELETE CASCADE
+    FOREIGN KEY (kidId) REFERENCES kids(id) ON DELETE CASCADE --if kid is deleted, attendance records are also deleted
 );
