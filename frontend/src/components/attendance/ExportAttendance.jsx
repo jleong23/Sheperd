6f10cec3f354dc4dd17da910d9fbb116d0986253
Jason Leong <jleongdh@gmail.com>
2026-04-27 12:51:09 +0930

    Refactor: Changed from export a csv file to exporting a proper xlsx file using XLSX package

--- frontend/src/components/attendance/ExportAttendance.jsx
+++ frontend/src/components/attendance/ExportAttendance.jsx
@@ -1,41 +1,50 @@
 /**
  * attendance/ExportAttendance.jsx
  */
-import { CSVLink } from "react-csv";
+import * as XLSX from "xlsx";
 import React from "react";
 
 export default function ExportAttendance({ attendance }) {
-  const headers = [
-    { label: "Name", key: "name" },
-    { label: "Term", key: "term" },
-    { label: "Week", key: "week" },
-    { label: "Status", key: "status" },
-    { label: "Reason", key: "reason" },
-  ];
+  const handleExport = () => {
+    if (!attendance || attendance.length === 0) return;
 
-  // Transform data to match
-  const csvData = attendance.map((record) => ({
-    name: record.name,
-    term: record.term,
-    week: record.week,
-    status: record.status,
-    reason: record.reason,
-  }));
-  // for debug purposes
-  console.log(csvData);
+    // 1.Transform data to match
+    const exportData = attendance.map((record) => ({
+      Name: record.name,
+      Term: record.term,
+      Week: record.week,
+      Status: record.status,
+      Reason: record.reason || "",
+    }));
+
+    // 2.Convert JSON -> worksheet
+
+    // 3.Create workbook
+    const workbook = XLSX.utils.book_new();
+
+    // 4.Append sheet
+    XLSX.utils.book_append_sheet(
+      workbook,
+      XLSX.utils.json_to_sheet(exportData),
+      "Attendance",
+    );
+
+    // 5.Export file
+    XLSX.writeFile(
+      workbook,
+      `Attendance-Term-${attendance[0]?.term}-Week-${attendance[0]?.week || "data"}.xlsx`,
+    );
+  };
 
   return (
     <>
       <div className="ml-auto">
-        <CSVLink
-          headers={headers}
-          data={csvData} // pass the transformed data array here
-          // Exported file name
-          filename={`Attendance-Week-${attendance[0]?.week || "data"}.csv`}
+        <button
+          onClick={handleExport}
           className="flex px-3 py-1 rounded-full bg-blue-50 text-blue-700 border border-blue-100 hover:bg-blue-200 text-sm font-medium"
         >
-          <button>Export Attendance</button>
-        </CSVLink>
+          Export Attendance
+        </button>
       </div>
     </>
   );
